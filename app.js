4936dc913467a4ad5c2380c128b1b0ca94ab16b3
pvstxy <116359910+pvstxy@users.noreply.github.com>
2024-07-22 13:45:31 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+window.addEventListener("DOMContentLoaded", event => {
+    console.log("DOM fully loaded and parsed")
+    const themeButton = document.querySelector(".btn")
+    const val = document.querySelector(".count")
+    newval = 0
+    const handleClick = () => {
+        newval++
+        document.getElementsByClassName('count')[0].textContent = newval;
+    }
+    themeButton.addEventListener("click", handleClick)
+  })
